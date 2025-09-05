ff5db4c4af6208044ff4bd007ac8a2416cb07a86
hoobasurf <brice.paneela@icloud.com>
2025-09-05 15:53:09 +0200

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,20 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAbI0fvcv9lIzpPViVBx2BlO8c6L3w-rbc",
+    authDomain: "affxx-copy.firebaseapp.com",
+    databaseURL: "https://affxx-copy-default-rtdb.firebaseio.com",
+    projectId: "affxx-copy",
+    storageBucket: "affxx-copy.firebasestorage.app",
+    messagingSenderId: "763273288933",
+    appId: "1:763273288933:web:d0083f5f6ab7284661c80c"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
