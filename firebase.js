8593e98cda33ad6b18939279ecd5d066a1b8ace0
hoobasurf <brice.paneela@icloud.com>
2025-09-05 18:23:18 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,20 +1,17 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyAbI0fvcv9lIzpPViVBx2BlO8c6L3w-rbc",
-    authDomain: "affxx-copy.firebaseapp.com",
-    databaseURL: "https://affxx-copy-default-rtdb.firebaseio.com",
-    projectId: "affxx-copy",
-    storageBucket: "affxx-copy.firebasestorage.app",
-    messagingSenderId: "763273288933",
-    appId: "1:763273288933:web:d0083f5f6ab7284661c80c"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyAbI0fvcv9lIzpPViVBx2BlO8c6L3w-rbc",
+  authDomain: "affxx-copy.firebaseapp.com",
+  databaseURL: "https://affxx-copy-default-rtdb.firebaseio.com",
+  projectId: "affxx-copy",
+  storageBucket: "affxx-copy.firebasestorage.app",
+  messagingSenderId: "763273288933",
+  appId: "1:763273288933:web:d0083f5f6ab7284661c80c"
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-</script>
+// Initialise Firebase
+const app = initializeApp(firebaseConfig);
+
+// 🔹 Important : exporter app pour pouvoir l’utiliser dans les autres fichiers
+export { app };
