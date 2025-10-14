08f3d7a7c493c988b85441c011f6f9c5ec6b0037
hoobasurf <brice.paneela@icloud.com>
2025-10-14 21:37:40 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,16 +1,23 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+import { getAuth, setPersistence, browserLocalPersistence } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAbI0fvcv9lIzpPViVBx2BlO8c6L3w-rbc",
   authDomain: "affxx-copy.firebaseapp.com",
   databaseURL: "https://affxx-copy-default-rtdb.firebaseio.com",
   projectId: "affxx-copy",
-  storageBucket: "affxx-copy.appspot.com",   // ✅ corrigé
+  storageBucket: "affxx-copy.appspot.com",
   messagingSenderId: "763273288933",
   appId: "1:763273288933:web:d0083f5f6ab7284661c80c"
 };
-// Initialise Firebase
+
+// Initialisation
 const app = initializeApp(firebaseConfig);
 
-// 🔹 Important : exporter app pour pouvoir l’utiliser dans les autres fichiers
-export { app };
+// 🔹 Persistance locale de la session
+const auth = getAuth(app);
+setPersistence(auth, browserLocalPersistence)
+  .then(() => console.log("✅ Persistance activée (session locale)"))
+  .catch(err => console.error("Erreur persistance :", err));
+
+export { app, auth };
